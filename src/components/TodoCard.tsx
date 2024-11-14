54f77109ec452c3abb3ea6ddecde7ebce76196d2
denisdiaconu <denisdiaconu117@yahoo.com>
2024-11-14 11:22:44 +0100

    Add delete logic

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -14,14 +14,23 @@ const TodoCard = ({ todo, todos, setTodos }: Props) => {
   const handleDone = (id:number) => {
     setTodos(todos.map((todo)=> todo.id===id?{...todo, isDone:!todo.isDone}: todo))
   }
+  const handleDelete = (id:number) => {
+    setTodos(todos.filter((todo) => todo.id!== id))
+  }
   return (
     <form className="todos_card">
-      <span className="todos_card_text">{todo.todo}</span>
+      {
+        todo.isDone? (
+          <s className="todos_card_text">{todo.todo}</s>
+        ): (
+          <span className="todos_card_text">{todo.todo}</span>
+        )
+      }
       <div>
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
         <span className="icon" onClick={() => handleDone(todo.id)}>
